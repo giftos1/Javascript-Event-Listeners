b6af2c019d9b2e8f3f6aabad3ea0f5dd5a10bdce
Giftos1 <giftsydney676@gmail.com>
2023-01-22 15:52:29 +1000

    use click and mouseover event listeners

--- dom.js
+++ dom.js
@@ -2,3 +2,24 @@
 
 
 // Event Listeners
+
+// element.addEventListener("click", function);
+
+const buttonTwo = document.querySelector('.btn-two');
+console.log(buttonTwo);
+
+function alertBtn() {
+    alert('I see you again hmm!');
+}
+
+buttonTwo.addEventListener('click', alertBtn);
+
+// Mouseover
+
+const newBackgroundColor = document.querySelector('.container');
+
+function changeBgColor() {
+    newBackgroundColor.style.backgroundColor = 'lightblue';
+}
+
+newBackgroundColor.addEventListener('mouseover', changeBgColor);
