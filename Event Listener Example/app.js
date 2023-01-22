b20d1499302eb6d55a2ef825e750e1fec79db52e
Giftos1 <giftsydney676@gmail.com>
2023-01-22 19:16:26 +1000

    create click event listener

---
+++ Event Listener Example/app.js
@@ -0,0 +1,19 @@
+const revealBtn = document.querySelector('.reveal-btn');
+const hiddenContent = document.querySelector('.hidden-content');
+const container = document.querySelector('.container');
+
+function revealContent() {
+    if (hiddenContent.classList.contains('reveal-btn')) {    
+        hiddenContent.classList.remove('reveal-btn');
+        revealBtn.style.width = '90%';
+        
+    } else {
+        hiddenContent.classList.add('reveal-btn');
+        revealBtn.style.width = 'fit-content';
+        container.style.flexDirection = 'row';
+        container.style.gap = '20px';
+    }
+
+}
+
+revealBtn.addEventListener('click', revealContent);
